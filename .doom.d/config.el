c6ddcad5d23ca1d1f4bf194f6ba67601644a24eb
syskay <syskay@gmail.com>
2021-03-02 00:16:06 +0100

    Emacs doom configuration

---
+++ .doom.d/config.el
@@ -0,0 +1,89 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "syskay"
+      user-mail-address "syskay@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; doom related
+;;(setq doom-theme 'doom-nord-light)
+(setq doom-theme 'leuven)
+
+;; fonts
+(setq doom-font (font-spec :family "Fira Code" :size 14)
+      doom-variable-pitch-font (font-spec :family "Fira Code" :size 14)
+      doom-big-font (font-spec :family "Fira Code" :size 24))
+
+;; org-mode related
+;; org-superstar
+(require 'org-superstar)
+(setq org-superstar-headline-bullets-list '("◉" "○" "✸" "✿" "○" "▷" "⁖"))
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+
+;; org-agenda
+(setq org-agenda-include-diary t)
+(setq org-log-done 'time)
+
+;; org-jira
+(require 'org-jira)
+
+;; ox-reveal
+(require 'ox-reveal)
+
+;;elfeed related
+(require 'elfeed-org)
+(setq rmh-elfeed-org-files
+      (list "~/elfeed/elfeed.org"))
+(elfeed-org)
+(require 'elfeed)
+(setq elfeed-db-directory "~/elfeed/elfeeddb")
