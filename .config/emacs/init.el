7c8f2a52e6beaeb1f38154a424eb8428c9a2c9ef
syskay <syskay@gmail.com>
2021-04-07 16:26:49 +0200

    update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -116,7 +116,7 @@
 
 (use-package org
   :config
-  (setq org-ellipsis " .")
+  (setq org-ellipsis " ▼")
   (setq org-hide-emphasis-markers t)
   )
 
@@ -316,16 +316,3 @@
 
 (use-package xresources-theme
   :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(xresources-theme evil-collection evil general doom-modeline command-log-mode rust-mode org-ac auto-complete flycheck all-the-icons-dired dired-single elfeed-dashboard elfeed-org org-re-reveal org-tree-slide emojify unicode-fonts which-key org-download org-superstar ivy-prescient counsel ivy-rich ivy dashboard page-break-lines doom-themes no-littering use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
