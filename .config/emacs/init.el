09f72a47d818a879e02a584e2572c7e6f78d84cb
syskay <syskay@gmail.com>
2021-06-07 15:36:11 +0200

    emacs: elfeed default search filter update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -359,7 +359,8 @@
 (use-package elfeed
   :ensure t
   :config
-  (setq elfeed-db-directory "~/shared/elfeed/elfeeddb"))
+  (setq elfeed-db-directory "~/shared/elfeed/elfeeddb")
+  (setq-default elfeed-search-filter "@6-months-ago "))
 
 (use-package elfeed-dashboard
   :ensure t
@@ -456,23 +457,3 @@
 
 (use-package xresources-theme
   :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-minibuffer-history-key "M-p")
- '(package-selected-packages
-   '(ivy-prescient xresources-theme which-key use-package unicode-fonts undo-tree undo-fu rust-mode rfc-mode ranger org-tree-slide org-superstar org-super-agenda org-roam org-re-reveal org-download org-ac ivy-rich helm-org-rifle general flycheck evil-org evil-goggles evil-collection emojify elfeed-org elfeed-dashboard doom-themes doom-modeline dired-single dashboard counsel command-log-mode beacon all-the-icons-ivy all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-goggles-change-face ((t (:inherit diff-removed))))
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
